3aae3c0b30c5d68e2cd57e8f9a9631d9fda1e2d7
gaydeym <gay20001@byui.edu>
2022-01-16 22:44:53 -0300

    fixing some errors

--- js/currenttime.js
+++
@@ -1,3 +0,0 @@
-// MM/DD/YYYY HH:MM:SS
-const options = {month: "numeric", day: "numeric", year: "numeric", hour: "numeric", minute: "numeric", second: "numeric"};
-document.getElementById("currenttime").textContent = new Date().toLocaleDateString("en-US", options);
