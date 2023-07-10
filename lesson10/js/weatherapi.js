85369ab584a9de6ec96b89b46bbfbe80b07648e5
gaydeym <gay20001@byui.com>
2023-07-10 21:42:48 +1200

    lesson10

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,34 @@
+const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=bf5292bf7f04806d4f8e644e2e480fbf";
+
+// select HTML elements in the document
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(1)}</strong>`;
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+
+    weatherIcon.setAttribute("src", iconsrc);
+    weatherIcon.setAttribute("alt", desc);
+    captionDesc.textContent = desc;
+}
+
+apiFetch();
