515fac774863c3e07e0587181002352d1db19417
gaydeym <gay20001@byui.com>
2023-07-11 14:22:15 +1200

    Week 10

---
+++ chamber/js/weatherapi.js
@@ -0,0 +1,36 @@
+const url =
+    "https://api.openweathermap.org/data/2.5/weather?lat=-34.7609&lon=-58.4063&units=imperial&appid=bf5292bf7f04806d4f8e644e2e480fbf";
+
+// select HTML elements in the document
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const weatherCondition = document.querySelector("#weather-condition");
+const windSpeed = document.querySelector("#wind-speed");
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(weatherData) {
+    currentTemp.innerHTML = weatherData.main.temp.toFixed(0);
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+
+    weatherIcon.setAttribute("src", iconsrc);
+    weatherIcon.setAttribute("alt", desc);
+    weatherCondition.textContent = desc;
+    windSpeed.textContent = `${weatherData.wind.speed}  mph`;
+}
+
+apiFetch();
--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,11 +1,11 @@
-const temperature = parseFloat(document.getElementById('temperature').textContent);
+const temperature = parseFloat(document.getElementById('current-temp').textContent);
 const weather_condition = document.getElementById('weather-condition').textContent;
 const wind_speed = parseFloat(document.getElementById('wind-speed').textContent);
 const wind_chill = document.getElementById('wind-chill');
 
 if (temperature <= 50 && wind_speed > 3.0) {
     // Formula for calculating wind chill value
-    wind_chill.textContent = (35.74 + 0.6215 * temperature - 35.75 * Math.pow(wind_speed, 0.16) + 0.4275 * temperature * Math.pow(wind_speed, 0.16)).toFixed(2);
+    wind_chill.textContent = `${(35.74 + 0.6215 * temperature - 35.75 * Math.pow(wind_speed, 0.16) + 0.4275 * temperature * Math.pow(wind_speed, 0.16)).toFixed(2)} °F`;
 } else {
     wind_chill.textContent = "N/A";
 };
