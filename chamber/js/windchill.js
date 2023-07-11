c23692857f32baa6b85c88a77609c70e2707464d
gaydeym <gay20001@byui.com>
2023-07-11 20:54:35 +1200

    Week 11

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -5,7 +5,7 @@ const wind_chill = document.getElementById('wind-chill');
 
 if (temperature <= 50 && wind_speed > 3.0) {
     // Formula for calculating wind chill value
-    wind_chill.textContent = `${(35.74 + 0.6215 * temperature - 35.75 * Math.pow(wind_speed, 0.16) + 0.4275 * temperature * Math.pow(wind_speed, 0.16)).toFixed(2)} °F`;
+    wind_chill.textContent = `${(35.74 + 0.6215 * temperature - 35.75 * Math.pow(wind_speed, 0.16) + 0.4275 * temperature * Math.pow(wind_speed, 0.16)).toFixed(2)}°F`;
 } else {
     wind_chill.textContent = "N/A";
 };
